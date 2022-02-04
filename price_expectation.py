7b74bd9ee2fa9728f76921a6cd648f12be6e8f81
StarBlossom99 <hansw1019@naver.com>
2022-02-04 23:42:31 +0900

    0204

--- price_expectation.py
+++ price_expectation.py
@@ -2,29 +2,52 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from prophet import Prophet
+import logging
+import seaborn as sns
+from fbprophet.plot import plot_plotly, plot_components_plotly
 # from statsmodels.tsa.arima_model import ARIMA
 # import statsmodels.api as sm
 
-data = pd.read_csv("C:/CRAproject/today_20220128/005930.csv", parse_dates=['일자'], dtype={'종목코드' : str}, header = 0, index_col=0, squeeze=True)
+data = pd.read_csv("C:/CRAproject/today_20220203/005930.csv", parse_dates=['일자'], dtype={'종목코드' : str}, header = 0, index_col=0, squeeze=True)
 data = data.fillna(0)
-data = data.loc[:,['일자', '현재가']]
+data = data.loc[:,['일자', '현재가', '시가', '고가', '저가', '거래량']]
 print(data)
-data.rename(columns= {'현재가':'y', '일자':'ds'}, inplace=True)
-# data.set_index('day', inplace=True)
+data.rename(columns= {'현재가':'y', '일자':'day', '시가':'open', '고가':'high', '저가':'low', '거래량':'volume'}, inplace=True)
+data['ds'] = data['day'].apply(lambda x : x)
+print(data)
+data.set_index('day', inplace=True)
+
 print(data)
 
+plt.figure(figsize=(16, 9))
+sns.lineplot(x=data.index, y='y', data=data)
+plt.show()
 
-prophet = Prophet(seasonality_mode = 'multiplicative',
-                 yearly_seasonality=True, 
-                 weekly_seasonality=True,
-                 daily_seasonality=True,
-                 changepoint_prior_scale=0.5)
+logger = logging.getLogger('fbprophet')
+logger.setLevel(logging.DEBUG)
 
-prophet.fit(data)
+m = Prophet()
+print(m.stan_backend)
+# model = Prophet()
+# model.fit(data)
 
-future_data = prophet.make_future_dataframe(periods = 5, freq = 'd')
-forecast_data = prophet.predict(future_data)
-forecast_data[['ds','yhat', 'yhat_lower', 'yhat_upper']].tail(5)
+# future = model.make_future_dataframe(periods=30)
+# print(future)
+
+# prophet = Prophet(seasonality_mode = 'multiplicative',
+#                  yearly_seasonality=True, 
+#                  weekly_seasonality=True,
+#                  daily_seasonality=True,
+#                  changepoint_prior_scale=0.5)
+
+# prophet.fit(data)
+
+# future_data = prophet.make_future_dataframe(periods = 5, freq = 'd')
+# forecast_data = prophet.predict(future_data)
+# forecast_data[['ds','yhat', 'yhat_lower', 'yhat_upper']].tail(5)
+
+
+
 # plt.xlabel("일자")
 # plt.ylabel("현재가")
 # ax = plt.gca()
